c2f0519511f63acbfec738088dc935c56d5853e7
ghigbie <gehigbie@gmail.com>
2021-11-29 20:23:15 -0500

    Add new routes and veiws

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root to: "courses#index"
+  get 'courses/new', to: 'courses#new'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'contactus', to: 'pages#contact'
 end
